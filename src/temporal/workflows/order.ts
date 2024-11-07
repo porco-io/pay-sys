32d100e3dd9c93c3eb8cc63f8ce24aeb6f91a4b7
ZackG <zacker.ping@gmail.com>
2024-11-07 18:40:29 +0800

    update: 等待支付检查设置为8秒

--- src/temporal/workflows/order.ts
+++ src/temporal/workflows/order.ts
@@ -25,13 +25,14 @@ export const orderProcessingWorkflow = async (orderSn: string) => {
     const {
       success,
       shouldClose,
-    } = await executeChild(checkPaymentWorkflow, {
+    } = await executeChild(waitPaymentWorkflow, {
       args: [orderSn],
+      workflowId: `waitPayment-${orderSn}`,
     });
     // 支付失败或超时或订单被关闭
     if (!success) {
       if (shouldClose) {
-        await executeChild(closeOrderWorkflow, { args: [orderSn] });
+        await executeChild(closeOrderWorkflow, { args: [orderSn], workflowId: `closeOrder-${orderSn}` });
       }
       return;
     }
@@ -46,22 +47,22 @@ export const orderProcessingWorkflow = async (orderSn: string) => {
     case OrderProcessType.auto:
       {
         /// 进入发货流程
-        await executeChild(shipOrderWorkflow, { args: [orderSn] });
+        await executeChild(shipOrderWorkflow, { args: [orderSn], workflowId: `ship-${orderSn}` });
         /// 进入订单完成流程
-        await executeChild(completeOrderWorkflow, { args: [orderSn] });
-        console.log(`订单(${orderSn})流程已结束!`);
+        await executeChild(completeOrderWorkflow, { args: [orderSn], workflowId: `complete-${orderSn}` });
+        console.log(`订单流程已结束!`);
       }
       break;
     // 手动发货订单
     case OrderProcessType.manual:
       {
         // 进入准备发货流程
-        await executeChild(preparingOrderWorkflow, { args: [orderSn] });
+        await executeChild(preparingOrderWorkflow, { args: [orderSn], workflowId: `preparing-${orderSn}` });
         // 进入等待收货流程
-        await executeChild(receivingOrderWorkflow, { args: [orderSn] });
+        await executeChild(receivingOrderWorkflow, { args: [orderSn], workflowId: `receiving-${orderSn}` });
         // 进入订单完成流程
-        await executeChild(completeOrderWorkflow, { args: [orderSn] });
-        console.log(`订单(${orderSn})流程已结束!`);
+        await executeChild(completeOrderWorkflow, { args: [orderSn], workflowId: `complete-${orderSn}` });
+        console.log(`订单流程已结束!`);
       }
       break;
   }
@@ -70,17 +71,17 @@ export const orderProcessingWorkflow = async (orderSn: string) => {
 };
 
 /** 检查订单支付状态流程 */
-export const checkPaymentWorkflow = async (
+export const waitPaymentWorkflow = async (
   orderSn: string
 ): Promise<{ success: boolean; shouldClose: boolean }> => {
-  console.log(`[订单${orderSn}]等待支付流程开始`);
+  console.log(`订单等待支付流程开始`);
   const timeoutDuration = 1000 * 60 * 60 * 2; // 2 hours
   while (true) {
     try {
       const order = await getOrderPaidResult(orderSn);
       const createTime = new Date(order.createdAt).getTime();
       if (!order) {
-        console.log(`[订单${orderSn}]不存在`);
+        console.log(`订单不存在`);
         return {
           success: false,
           shouldClose: false,
@@ -88,7 +89,7 @@ export const checkPaymentWorkflow = async (
       }
       /// 订单已关闭
       if (order.state === OrderState.closed) {
-        console.log(`[订单${orderSn}]已关闭`);
+        console.log(`订单已关闭`);
         return {
           success: false,
           shouldClose: true,
@@ -96,37 +97,37 @@ export const checkPaymentWorkflow = async (
       }
       if (order.payTime) {
         // 已支付
-        console.log(`[订单${orderSn}]已支付, 准备进入下一个流程。`);
+        console.log(`订单已支付, 准备进入下一个流程。`);
         return {
           success: true,
           shouldClose: false,
         };
       } else if (Date.now() - createTime >= timeoutDuration) {
-        console.log(`[订单${orderSn}]支付超时, 订单将自动关闭`);
+        console.log(`订单支付超时, 订单将自动关闭`);
         // 超时未支付
         return {
           success: false,
           shouldClose: true,
         };
       }
-      // 两秒后再检查订单支付状态
-      await sleep(2000);
+      // 8秒后再检查订单支付状态
+      await sleep(8000);
     } catch (e) {
-      console.log(`[订单${orderSn}]支付状态查询失败，将进行重试。`);
-      await sleep(2000);
+      console.log(`订单支付状态查询失败，将进行重试。`);
+      await sleep(8000);
     }
   }
 };
 
 /** 关闭订单工作流 */
 export const closeOrderWorkflow = async (orderSn: string) => {
-  console.log(`开始关闭订单(${orderSn})工作流`);
+  console.log(`开始关闭订单工作流`);
   return closeOrder(orderSn);
 };
 
 /** 完成订单工作流 */
 export const completeOrderWorkflow = async (orderSn: string) => {
-  console.log(`开始完成订单(${orderSn})工作流`);
+  console.log(`开始完成订单工作流`);
   return completeOrder(orderSn);
 };
 
@@ -140,7 +141,7 @@ export const preparingOrderWorkflow = async (orderSn: string) => {
   await prepearShipOrder(orderSn);
   // TODO: 准备发货，直到超时异常
   while (true) {
-    console.log(`[订单${orderSn}]已发货`);
+    console.log(`订单已发货`);
     break;
   }
 };
@@ -149,7 +150,7 @@ export const preparingOrderWorkflow = async (orderSn: string) => {
 export const receivingOrderWorkflow = async (orderSn: string) => {
   // TODO: 等待收货，直到超时，进行自动收货
   while (true) {
-    console.log(`[订单${orderSn}]已确认收货`);
+    console.log(`订单已确认收货`);
     break;
   }
 };
