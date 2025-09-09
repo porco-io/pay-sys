7c2f78049003e090854e1027277fb36962eb7383
kino <kino@kino-mbp.local>
2025-09-09 22:16:37 +0800

    fix: 修复支付回调接口调不通

--- src/controller/pay.controller.ts
+++ src/controller/pay.controller.ts
@@ -1,21 +1,30 @@
-import { Inject, Controller, Get, Query, Post, Body, Patch, httpError, Param, Del } from '@midwayjs/core';
-import { Context } from '@midwayjs/koa';
-import { WxPayCallbackDTO } from '../dto/pay.dto';
-import { PayService } from '../service/pay.service';
-import { AliService } from '../service/ali.service';
-import { MidwayLogger } from '@midwayjs/logger';
-import { PayState } from '../define/enums';
-import { LoginRequired } from '../middleware/auth.middleware';
+import {
+  Inject,
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Patch,
+  httpError,
+  Param,
+  Del,
+} from "@midwayjs/core";
+import { Context } from "@midwayjs/koa";
+import { WxPayCallbackDTO } from "../dto/pay.dto";
+import { PayService } from "../service/pay.service";
+import { AliService } from "../service/ali.service";
+import { MidwayLogger } from "@midwayjs/logger";
+import { PayState } from "../define/enums";
+import { LoginRequired } from "../middleware/auth.middleware";
 
-@Controller('/api/pay', {
-  middleware: [LoginRequired]
-})
+@Controller("/api/pay", {})
 export class PayController {
   @Inject()
   ctx: Context;
 
   @Inject()
-  logger: MidwayLogger
+  logger: MidwayLogger;
 
   @Inject()
   payService: PayService;
@@ -24,11 +33,14 @@ export class PayController {
   aliService: AliService;
 
   // 获取微信支付参数
-  @Get('/:paySn/wxpayParams', { description: '获取微信支付参数' })
-  async getPayParams(@Param('paySn') paySn: string) {
+  @Get("/:paySn/wxpayParams", {
+    description: "获取微信支付参数",
+    middleware: [LoginRequired],
+  })
+  async getPayParams(@Param("paySn") paySn: string) {
     const payOrder = await this.payService.findByPaySn(paySn);
     if (!payOrder) {
-      throw new httpError.BadRequestError('支付单不存在');
+      throw new httpError.BadRequestError("支付单不存在");
     }
     // 如果不是正在支付的支付单，则抛错
     const stateErrMsg = payOrder.matchState(PayState.paying);
@@ -40,16 +52,24 @@ export class PayController {
   }
 
   // 微信支付回调
-  @Post('/wxCallback/:paySn', {
-    description: '微信支付回调(只有支付成功才有回调)',
+  @Post("/wxCallback/:paySn", {
+    description: "微信支付回调(只有支付成功才有回调)",
   })
-  async handleWxPayCallback(@Param('paySn') paySn: string, @Body() params: WxPayCallbackDTO) {
-    this.logger.info('handleWxPayCallback - paySn: ', paySn, '\n', JSON.stringify(params));
+  async handleWxPayCallback(
+    @Param("paySn") paySn: string,
+    @Body() params: WxPayCallbackDTO
+  ) {
+    this.logger.info(
+      "handleWxPayCallback - paySn: ",
+      paySn,
+      "\n",
+      JSON.stringify(params)
+    );
     // 处理支付回调
     try {
       const payOrder = await this.payService.findByPaySn(paySn);
       if (!payOrder) {
-        throw new httpError.BadRequestError('支付单不存在');
+        throw new httpError.BadRequestError("支付单不存在");
       }
       await this.payService.handleWxPayCallback(payOrder, params);
     } catch (err) {
@@ -59,11 +79,14 @@ export class PayController {
   }
 
   // 获取支付宝-支付参数
-  @Get('/:paySn/alipayParams', { description: '获取支付宝支付参数' })
-  async getAliPayParams(@Param('paySn') paySn: string) {
+  @Get("/:paySn/alipayParams", {
+    description: "获取支付宝支付参数",
+    middleware: [LoginRequired],
+  })
+  async getAliPayParams(@Param("paySn") paySn: string) {
     const payOrder = await this.payService.findByPaySn(paySn);
     if (!payOrder) {
-      throw new httpError.BadRequestError('支付单不存在');
+      throw new httpError.BadRequestError("支付单不存在");
     }
     // 如果不是正在支付的支付单，则抛错
     const stateErrMsg = payOrder.matchState(PayState.paying);
@@ -76,11 +99,14 @@ export class PayController {
   }
 
   // 支付宝支付回调
-  @Post('/alipayCallback/:paySn', {
-    description: '支付宝支付回调(只有支付成功才有回调)',
+  @Post("/alipayCallback/:paySn", {
+    description: "支付宝支付回调(只有支付成功才有回调)",
   })
-  async handleAliPayCallback(@Param('paySn') paySn: string, @Body() params: any) {
-    console.log('params: ', params);
+  async handleAliPayCallback(
+    @Param("paySn") paySn: string,
+    @Body() params: any
+  ) {
+    console.log("params: ", params);
     return true;
   }
 }
